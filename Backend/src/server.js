6d4a141fd367e6dcdc316a570c0a58ff698b00c7
Kavin <kavinmadusanka20011@gmail.com>
2026-01-05 11:05:38 +0530

    initial set up

---
+++ Backend/src/server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import connectDB from './config/database.js';
+
+dotenv.config();
+
+connectDB()
+
+const app = express();
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send({
+    message: "Welcome to Personal Expense Tracker",
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server Running on ${process.env.DEV_MODE} mode`.bgCyan.white);
+    console.log(`Server is running on port ${PORT}`.bgCyan.white)
+});
